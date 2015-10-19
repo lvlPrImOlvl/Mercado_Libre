a2c0e51d2e6b9758c0534ea3cfeb4173c6cfcb0d
PrImO <baalzephon_evil_raising@hotmail.com>
2015-10-18 23:21:04 -0500

    m

--- src/java/Articulo.java
+++ src/java/Articulo.java
@@ -19,9 +19,9 @@ public class Articulo {
 	}
 
 	public static void setArtToLista(ArrayList<Articulo> articulo){
-		articulo.add(new Producto("Pantalla LED", "50 pulgadas full HD",(float)10500));
-		articulo.add(new Producto("Estereo SONY", "Sonido envolvente 7.1 canales",(float)8500));
-		articulo.add(new Producto("XBOX ONE", "Halo edition",(float)7500));
+		articulo.add(new Articulo("Pantalla LED", "50 pulgadas full HD",(float)10500));
+		articulo.add(new Articulo("Estereo SONY", "Sonido envolvente 7.1 canales",(float)8500));
+		articulo.add(new Articulo("XBOX ONE", "Halo edition",(float)7500));
 		return;
 	}
     
